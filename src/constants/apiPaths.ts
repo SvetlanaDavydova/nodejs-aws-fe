ae756dd91c7efcb06555ed3fb263414c3d7b815a
DESKTOP-AL6PEAQ\Stas <svetlana.davydova@whitesharx.com>
2021-09-14 17:05:28 +0300

    path for products page enabled

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://q5vp22pgsa.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://q5vp22pgsa.execute-api.eu-west-1.amazonaws.com/dev/products',
+  bff: 'https://hf9i1smw2i.execute-api.eu-west-1.amazonaws.com/dev/products',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
